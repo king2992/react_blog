a5cf3bbcffb45a93b8a0fb6a397fc32aae91756d
LJW <king2992@naver.com>
2023-06-11 22:24:26 +0900

    글 목록, 좋아요 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+let [postTitle, setPostTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+let [likeCnt, setLikeCnt] = useState(0);
+
+function likeUp(){
+  setLikeCnt(likeCnt + 1);
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <h4>reactBlog</h4>
+      </div>
+      <button onClick={() => {
+        let copy = [...postTitle];
+        copy[0] = '여자 코트 추천';
+        setPostTitle(copy)
+      }}>글수정</button>
+      <div className='list'>
+        <h4>{postTitle[0] } <span onClick={likeUp}>👍</span> {likeCnt} </h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
