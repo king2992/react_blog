4743ccd7bec24133abbdaf1d19f7326110741f73
LJW <king2992@naver.com>
2023-06-12 23:06:39 +0900

    글 상세보기 추가, 프롭스

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import {useState} from 'react';
 function App() {
 
 let [postTitle, setPostTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
-let [likeCnt, setLikeCnt] = useState(0);
+let [likeCnt, setLikeCnt] = useState([0,0,0]);
+let [modal, setModal] = useState(false);
+let [curPost, setCurPost] = useState();
 
-function likeUp(){
-  setLikeCnt(likeCnt + 1);
+
+function likeUp(like){
+  setLikeCnt(like);
+}
+
+function selectPost(selected){
+  setCurPost(selected)
+  modal === true ? setModal(false) : setModal(true)
 }
 
 
@@ -21,12 +29,39 @@ function likeUp(){
         copy[0] = '여자 코트 추천';
         setPostTitle(copy)
       }}>글수정</button>
-      <div className='list'>
-        <h4>{postTitle[0] } <span onClick={likeUp}>👍</span> {likeCnt} </h4>
-        <p>2월 17일 발행</p>
-      </div>
+      {
+        postTitle.map(function(a, i){
+          return (
+            <div className='list' key={i}>
+              <h4 onClick={() => {selectPost(a)}}>
+                {postTitle[i] } 
+                <span onClick={() =>{
+                  let copy = [...likeCnt]
+                  copy[i] = copy[i] + 1;
+                  setLikeCnt(copy)
+                }
+                }>👍</span> {likeCnt[i]} 
+              </h4>
+              <p>2월 17일 발행</p>
+            </div>
+          )
+        })
+      }
+      {
+        modal === true ? <Modal postTitle={curPost}></Modal> : null
+      }
     </div>
   );
+}
+
+function Modal(props){
+  return(
+    <div className='modal'>
+      <h4>{props.postTitle}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
